0aa218fc25fa0903f7780f3c552e13925efdbdb2
shubham <gshubham030@gmail.com>
2019-12-29 07:12:29 +0530

    removed unwanted data and apk file added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:event_page/ui/screens/event/components/single_event_screen.dart';
 import 'package:event_page/ui/screens/page_router.dart';
 import 'package:flutter/material.dart';
 
